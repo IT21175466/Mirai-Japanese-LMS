51037002ff6c33773ae3f0635c4720ddcb09adf4
Chamath <chamath@flexiata.lk>
2024-04-19 15:10:14 +0530

    Splash Screen

---
+++ lib/constraints/app_colors.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColors {
+  //TextGray
+  static const Color textGrayColor = Color.fromARGB(255, 143, 143, 143);
+
+  // Button colors
+  static const Color buttonColor = Color.fromARGB(225, 44, 75, 157);
+
+  //TextBlackColor
+  static const Color textBlackColor = Color.fromARGB(255, 30, 28, 29);
+}

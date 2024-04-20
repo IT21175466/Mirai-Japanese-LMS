530118a95444b68950aebf136c2e3c78a40bc6d5
Chamath <chamath@flexiata.lk>
2024-04-20 12:03:00 +0530

    Update app_colors.dart

--- lib/constraints/app_colors.dart
+++ lib/constraints/app_colors.dart
@@ -5,7 +5,7 @@ class AppColors {
   static const Color textGrayColor = Color.fromARGB(255, 143, 143, 143);
 
   // Accent colors
-  static const Color accentColor = Color.fromARGB(225, 44, 75, 157);
+  static const Color accentColor = Color.fromARGB(225, 29, 31, 105);
 
   // Low Accent colors
   static const Color lowAccentColor = Color.fromARGB(225, 232, 241, 255);

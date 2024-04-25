b5575f59e50ce506f11f65583df8868ea7c5e9a3
Chamath <chamath@flexiata.lk>
2024-04-25 18:55:00 +0530

    Update home_tab.dart

--- lib/views/home/tabs/home_tab.dart
+++ lib/views/home/tabs/home_tab.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:miraijapanese/constraints/app_colors.dart';
 import 'package:miraijapanese/providers/app_data/app_data_provider.dart';
+import 'package:miraijapanese/providers/quiz/question_provider.dart';
+import 'package:miraijapanese/widgets/question_card.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:provider/provider.dart';
@@ -17,9 +20,9 @@ class _HomeTabState extends State<HomeTab> {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    return Consumer(
-      builder: (BuildContext context, AppDataProvider appDataProvider,
-              Widget? child) =>
+    return Consumer2(
+      builder: (BuildContext context, QuestionProvider questionProvider,
+              AppDataProvider appDataProvider, Widget? child) =>
           Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: false,
@@ -391,15 +394,66 @@ class _HomeTabState extends State<HomeTab> {
                     ],
                   ),
                 ),
-                // QuestionCard(
-                //   quizAmount: '25',
-                //   image: AssetImage('assets/images/sporken.jpg'),
-                //   quizTitle: 'Spoken Quiz',
-                //   isLocked: false,
-                //   isCompleted: true,
-                //   score: 82.0,
-                // ),
-
+                Container(
+                  width: screenWidth,
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('Lessons')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (!snapshot.hasData) {
+                        return Text('No data available');
+                      } else {
+                        return QuestionCard(
+                          quizAmount: snapshot.data!.docs[appDataProvider
+                              .completedLessions.length]['LessonNo'],
+                          image: NetworkImage(snapshot.data!.docs[
+                                  appDataProvider.completedLessions.length]
+                              ['Image_Url']),
+                          quizTitle: snapshot.data!.docs[appDataProvider
+                              .completedLessions.length]['LessonTitle'],
+                          isLocked: false,
+                          isCompleted: false,
+                          score: 0.0,
+                        );
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  width: screenWidth,
+                  child: StreamBuilder(
+                    stream: FirebaseFirestore.instance
+                        .collection('PastPapers')
+                        .snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return Center(child: CircularProgressIndicator());
+                      } else if (snapshot.hasError) {
+                        return Text('Error: ${snapshot.error}');
+                      } else if (!snapshot.hasData) {
+                        return Text('No data available');
+                      } else {
+                        return QuestionCard(
+                          quizAmount: snapshot.data!.docs[appDataProvider
+                              .completedPastPapers.length]['LessonNo'],
+                          image: NetworkImage(snapshot.data!.docs[
+                                  appDataProvider.completedPastPapers.length]
+                              ['Image_Url']),
+                          quizTitle: snapshot.data!.docs[appDataProvider
+                              .completedPastPapers.length]['LessonTitle'],
+                          isLocked: false,
+                          isCompleted: false,
+                          score: 0.0,
+                        );
+                      }
+                    },
+                  ),
+                ),
                 SizedBox(
                   height: 20,
                 ),
